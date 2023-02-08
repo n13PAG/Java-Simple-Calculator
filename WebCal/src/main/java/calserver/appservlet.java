7ee0122ce3fb84dcf84dcac8ae26544fe7d58184
Binu Kandagedon <61514490+BinuBims@users.noreply.github.com>
2023-02-08 07:56:08 -0800

    finishing up front end
    able to send data from front end to the back end(java) as a string

---
+++ WebCal/src/main/java/calserver/appservlet.java
@@ -0,0 +1,49 @@
+package calserver;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class appservlet
+ */
+@WebServlet("/appservlet")
+public class appservlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public appservlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter pw = response.getWriter();
+		//string output from the front page(ex:"7+5"). parse and do your operations
+		String s = request.getParameter("answer");
+		pw.println("<h1>"+s);
+		pw.close();
+		
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
