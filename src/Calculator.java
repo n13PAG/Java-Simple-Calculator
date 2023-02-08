9fedbece630190ab343b55574d87fe27a3210382
n13PAG <74218503+n13PAG@users.noreply.github.com>
2023-02-07 18:32:04 -0800

    Merge Calculator and Calculator2 classes

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,6 +1,71 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Calculator {
+    private ArrayList<Double> operands;
+    private char operator;
+
+    /*  Selects number input from buttons.The value is inputted as type string, checked if numeric.
+        When a non-numeric input is received the value is parsed to type double and returned*/
+    public double selectNumber() {
+        Scanner input = new Scanner(System.in);
+        boolean inputOn = true;
+        String inputString = "";
+        while (inputOn) {
+            String buttonInput = input.next();
+            if (checkInput(buttonInput)) {
+                inputString += buttonInput;
+            }
+            else{
+                inputOn = false;
+            }
+        }
+        double operand = Double.parseDouble(inputString);
+        return operand;
+    }
+
+    /* selects operator. ***Scanner Needs to be replaced by button input */
+
+    public char selectOperator(){
+        Scanner input = new Scanner(System.in);
+        char selectedOperator = input.next().charAt(0);
+        return selectedOperator;
+    }
+
+    //Groups user input into a stored ArrayList?
+    public ArrayList<Double> parceUserInput(){
+        ArrayList<Double> inputArray= new ArrayList<Double>(2);
+        for(double inputValue: inputArray){
+            inputArray.add(selectNumber());
+        }
+
+        return inputArray;
+    }
+
+    /*Checks if input string value is numeric and returns true if can be parsed as double and false if nfe */
+    public boolean checkInput(String s){
+        if (s==null){
+            return false;
+        }
+        try{
+            double value = Double.parseDouble(s);
+        }catch (NumberFormatException nfe){
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Calculator2{" +
+                "operands=" + operands +
+                ", operator=" + operator +
+                ", cacheCapacity=" + cacheCapacity +
+                ", cachedResults=" + cachedResults +
+                ", currentResultsIndex=" + currentResultIndex +
+                '}';
+    }
+
     private int cacheCapacity;
     private ArrayList<Double> cachedResults;
     private int currentResultIndex;
@@ -40,7 +105,24 @@ public class Calculator {
     }
 
 //    public double makeTheOperationWithOldResult(double result){
-//        // ?
+//        switch (operator){
+//            case '+':
+//                break;
+//            case '-':
+//                break;
+//            case '*':
+//                break;
+//            case '/':
+//                break;
+//            case '^':
+//                break;
+//            case 'q':
+//                break;
+//            case '+':
+//                break;
+//            case '-':
+//                break;
+//        }
 //    }
 
     /* Adds result of calculation to the results cache. It removes the
--- src/LocalProgram.java
+++ src/LocalProgram.java
@@ -1,5 +1,24 @@
 public class LocalProgram{
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Calculator calculator = new Calculator();
+        calculator.setResultCacheCapacity(10);
+        double[] input = new double[]{1, 2, 3, 4, 5};
+        calculator.storeResult(2);
+        calculator.storeResult(3);
+        calculator.storeResult(4);
+        calculator.storeResult(5);
+        calculator.storeResult(6);
+
+        System.out.println(calculator.cycleToPreviousResult());
+        System.out.println(calculator.cycleToPreviousResult());
+        System.out.println(calculator.cycleToPreviousResult());
+        System.out.println(calculator.cycleToPreviousResult());
+        System.out.println(calculator.cycleToPreviousResult());
+        System.out.println(calculator.cycleToNextResult());
+        System.out.println(calculator.cycleToNextResult());
+        System.out.println(calculator.getLastResult());
+        System.out.println(calculator.cycleToNextResult());
+        System.out.println(calculator.cycleToNextResult());
+        System.out.println(calculator.cycleToNextResult());
     }
 }
